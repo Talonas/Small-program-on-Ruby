d5614c621ad8de6ee4e401ac7a7fdb31398a08ef
Paulius Pilkauskas <talonas@gmail.com>
2009-10-23 11:55:21 +0200

    updated album class and tests

--- Album.rb
+++ Album.rb
@@ -1,2 +1,13 @@
 class Album
+  
+  attr_accessor :id, :artist_id, :name, :year, :genre
+  
+  def initialize(id=nil, artist_id=nil, name=nil, year=nil, genre=nil)
+    @id = id
+    @artist_id = artist_id
+    @name = name
+    @year = year
+    @genre = genre
+  end
+  
 end
