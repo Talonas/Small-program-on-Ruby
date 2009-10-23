dad3ddae91782f017f38a68ee00d31a56462a4b9
Paulius Pilkauskas <talonas@gmail.com>
2009-10-23 17:12:22 +0200

    user test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -12,4 +12,16 @@ describe User do
     User.new(1, "Vardenis", "Pavardenis", "vyr", 99, "Klaipeda", "neturiu@as.pasto").should be_instance_of(User)
   end
   
+  it "should have id, name, surname, gender, age, adress and email" do
+    new_user = User.new(1, "Vardenis", "Pavardenis", "vyr", 99, "Klaipeda", "neturiu@pasto.as")
+    new_user.should be_instance_of(User)
+    new_user.id.should == 1
+    new_user.name.should == "Vardenis"
+    new_user.surname.should == "Pavardenis"
+    new_user.age.should == 99
+    new_user.gender.should == "vyr"
+    new_user.adress.should == "Klaipeda"
+    new_user.email.should == "neturiu@pasto.as"
+  end
+  
 end
