d7e991191bda047bf0783042fcb85cbbea09766c
Paulius Pilkauskas <talonas@gmail.com>
2009-10-23 17:09:37 +0200

    user class

--- User.rb
+++ User.rb
@@ -1,2 +1,15 @@
 class User
+  
+  attr_accessor :id, :name, :surname, :gender, :age, :adress, :email
+  
+  def initialize(id=nil, name=nil, surname=nil, gender=nil, age=nil, adress=nil, email=nil)
+    @id = id
+    @name = name
+    @surname = surname
+    @gender = gender
+    @age = age
+    @adress = adress
+    @email = email
+  end
+  
 end
