a53d2e38bc062205ba84afe4e692ab7d36287dd5
Paulius Pilkauskas <talonas@gmail.com>
2009-10-28 11:12:17 +0200

    some changes (should be_true)

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -6,20 +6,20 @@ describe Validation do
   
   describe Validation, "not_empty" do
     it "should success if not empty value" do
-      Validation.not_empty("value").should be_instance_of(TrueClass)
+      Validation.not_empty("value").should be_true
     end
     it "should fail if empty value" do
-      Validation.not_empty("").should be_instance_of(FalseClass)
+      Validation.not_empty("").should be_false
     end
   end
   
   describe Validation, "is_numeric" do
     it "should success if value is numeric" do
-      Validation.is_numeric(12).should be_instance_of(TrueClass)
-      Validation.is_numeric("12").should be_instance_of(TrueClass)
+      Validation.is_numeric(12).should be_true
+      Validation.is_numeric("12").should be_true
     end
     it "should fail if value is not numeric" do
-      Validation.is_numeric("labas").should be_instance_of(FalseClass)
+      Validation.is_numeric("labas").should be_false
     end
   end
   
