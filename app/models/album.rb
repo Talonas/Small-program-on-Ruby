a9410447a208c3ace23fd617a43f35c0d50f31c4
Paulius Pilkauskas <talonas@gmail.com>
2009-11-30 18:36:12 +0200

    modified app models

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,16 +4,20 @@ class Album < ActiveRecord::Base
 
   def sell user_id
     if self.store && self.store.amount > 0
-      if self.store.sell
-        UserHistory.create(
-          :user_id => user_id,
-          :album_id => @id,
-          :price => self.store.price
-        ).save
-        return true;
+      if self.store.sell && add_record_to_history(user_id)        
+        return true
       end
     end
     false
+  end
+
+  def add_record_to_history user_id
+    user_history = UserHistory.create(
+      :user_id => user_id,
+      :album_id => @id,
+      :price => self.store.price
+    )
+    user_history.save
   end
 
 end
--- app/models/user_history.rb
+++ app/models/user_history.rb
@@ -1,6 +1,6 @@
 class UserHistory < ActiveRecord::Base
 
-  has_one :user
+  has_one  :user
   has_many :albums
 
 end
